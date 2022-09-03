310e0d3bce279db46230bec9373636d105ad8f4f
CorinaMarisol <109838460+CorinaMarisol@users.noreply.github.com>
2022-09-02 22:04:34 -0600

    Add files via upload

---
+++ UniversidadUMG/Apps/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+class HomeView(TemplateView):
+    template_name='home.html'
+class EstudiantesView(TemplateView):
+    template_name='estudiantes.html'    
+class AdministradoresView(TemplateView):
+    template_name='administradores.html' 
+class AcercadeView(TemplateView):
+   template_name='acercade.html'    
+
