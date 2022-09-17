81e619a60a15ffd66a77d0429506dcc7059f59ff
CorinaMarisol <109838460+CorinaMarisol@users.noreply.github.com>
2022-09-16 23:25:05 -0600

    Add files via upload

--- UniversidadUMG/Apps/home/models.py
+++ UniversidadUMG/Apps/home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante (models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=200)
+    creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' %(self.nombre,self.apellido)
+
+
+class Publicaciones (models.Model):
+    Titulo = models.CharField(max_length=100)
+    Noticia = models.CharField(max_length=100)
+    Informacion = models.CharField(max_length=300)
+    creacion = models.DateTimeField(auto_now_add=True)
+    Estudiante = models.ManyToManyField(Estudiante)
+
+    def __str__(self):
+        return '%s %s %s' %(self.Titulo,self.Noticia,self.Informacion)
+
+
+class Comentarios (models.Model):
+    nombre = models.CharField(max_length=100)
+    comentario = models.CharField(max_length=300)
+    publicacion = models.CharField(max_length=200)
+    creacion = models.DateTimeField(auto_now_add=True)
+    Estudiante = models.ManyToManyField(Estudiante)
+
+    def __str__(self):
+         return '%s %s %s' %(self.comentario,self.comentario,self.publicacion)
